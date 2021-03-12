882d411db9fab33c824e9d3c447e76a3f03cdf6d
Marc Pomar <marc@corecode.school>
2021-03-12 18:36:49 +0100

    fix: release

---
+++ scripts/update-endpoints/fetch-json.js
@@ -0,0 +1,55 @@
+const { writeFileSync } = require("fs");
+const path = require("path");
+const graphqlGot = require("graphql-got");
+const prettier = require("prettier");
+
+if (!process.env.VERSION) {
+  throw new Error(`VERSION environment variable must be set`);
+}
+
+const version = process.env.VERSION.replace(/^v/, "");
+
+const QUERY = `
+query{
+  endpoints{
+    method
+    url
+    documentationUrl
+    parameters {
+      alias
+      deprecated
+      in
+      name
+    }
+  }
+}
+`;
+
+main();
+
+async function main() {
+  // const { endpoints } = await graphql(QUERY, {
+  //   //url: "http://localhost:3000/api/graphql",
+  //   version,
+  //   ignoreChangesBefore: "2020-06-10",
+  // });
+
+  const {
+    body: { endpoints },
+  } = await graphqlGot("http://localhost:3000/api/graphql", {
+    query: QUERY,
+    variables: {
+      //url: "http://localhost:3000/api/graphql",
+      version,
+      ignoreChangesBefore: "2020-06-10",
+    },
+  });
+  console.log(endpoints);
+  console.log("THE ENDPOINTS");
+  writeFileSync(
+    path.resolve(__dirname, "generated", "endpoints.json"),
+    prettier.format(JSON.stringify(endpoints), {
+      parser: "json",
+    })
+  );
+}
